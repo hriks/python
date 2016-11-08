a521387e447276478d648efa76a606e43726402c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-11-08 13:43:58 +0530

    Create Area_of_the_polygon.py

---
+++ Area_of_the_polygon.py
@@ -0,0 +1,9 @@
+import math
+def polygon(sides, length):
+    pie = 3.14159
+    nume = sides * (length ** 2)
+    deno = 4 * math.tan ( pie / sides)
+    val = nume / deno
+    return val
+    
+print polygon(7, 3
